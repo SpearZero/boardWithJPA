3c2dae143663c8a4a440dea710b60db7822e2e27
SpearZero <spearzero0@gmail.com>
2020-03-04 23:23:41 +0900

    글 쓰기 코드 수정
    컨트롤러와 서비스 코드 수정

--- src/main/java/jpa/board/controller/BoardController.java
+++ src/main/java/jpa/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,16 +18,19 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jpa.board.dto.BoardList;
+import jpa.board.dto.Content;
 import jpa.board.entity.Board;
 import jpa.board.entity.Member;
 import jpa.board.persistence.BoardRepository;
 import jpa.board.service.BoardService;
+import jpa.board.service.MemberService;
 import jpa.board.util.ParameterUtils;
 
 @Controller
@@ -37,14 +41,17 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@Autowired
+	private MemberService meberService;
+	
+	@Autowired
 	private BoardRepository boardRepo;
 	
     @GetMapping("")
     public String goBoard(HttpServletRequest request, Model model) {
     	
     	try {
-    		String requestPage = request.getParameter("page");
-    		int page = StringUtils.isEmpty(requestPage) ? 1 : Integer.parseInt(requestPage);
+    		String reqPage = ParameterUtils.getReqParameter(request, "page", "1");
+    		int page = Integer.parseInt(reqPage);
     		
     		List<BoardList> boardList = boardService.getBoardList(page);
     		List<String> pageList = boardService.getPagination(page);
@@ -111,15 +118,55 @@ public class BoardController {
     
     @GetMapping("/write")
     public String getWriteBoard(HttpServletRequest request, Model model) {
-    	String page = StringUtils.isEmpty(request.getParameter("page")) ? "1" : request.getParameter("page");
-    	model.addAttribute("page", page);
+    	String page = ParameterUtils.getReqParameter(request, "page", "1");
+    	String goPage = "/board?page="+page;
     	
-    	return boardService.getWriteBoard(request);
+    	try {
+    		HttpSession session = request.getSession(false);
+    		
+    		if(session != null) {
+    			Member member = Optional.ofNullable((Member)request.getSession().getAttribute("member")).orElse(null);
+    			goPage = member != null ? "board/writeBoard" : goPage;
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+		}
+    	
+    	model.addAttribute("page", page);
+    	return goPage;
     }
     
     @PostMapping("/write/boardContent")
-    public String postWriteBoard(HttpServletRequest request) {
-    	return boardService.postWriteBoard(request);
+    public String writeBoardContent(HttpServletRequest request, @ModelAttribute Content content) {
+    	String goPage = "redirect:/board";
+    	
+    	try {
+    		HttpSession session = request.getSession(false);
+    		
+    		if(session == null) {
+    			return goPage;
+    		}
+    		
+			Member sessionMember = Optional.ofNullable((Member)request.getSession().getAttribute("member")).orElse(null);
+			
+			if(sessionMember == null) {
+				return goPage;
+			}
+			
+			Optional<Member> optMember = meberService.findMember(sessionMember.getId());
+			Member member = optMember.orElse(null);
+			
+			if(member == null) {
+				return goPage;
+			}
+			
+			boardService.writeBoardContent(member, content);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+		}
+    	
+    	
+    	return goPage;
     }
     
     @GetMapping("/update")
